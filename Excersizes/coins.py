bd787a03e3fa63927640f72ac77f6635458a39e5
AmrKhalifa <amr.khalifa.m.a@gmail.com>
2020-07-02 09:31:22 +0200

    started chapter 7

--- Excersizes/coins.py
+++
@@ -1,9 +0,0 @@
-coins = [1, 2, 5, 10]
-amount = 13 
-
-
-sol_arr = [i for i in range(amount)]
-coins = sorted(coins, reverse=True)
-
-for element in sol_arr:
-
---
+++ Excersizes/lists.py
@@ -0,0 +1,11 @@
+# insertion sort for an array
+
+arr = [3, 1, 8, 5, 2, 7, 10, 9, 4, 6] 
+print(f'original array {arr}')
+
+for boundary_idx in range(len(arr)):
+	for cur_idx in range(boundary_idx, 0, -1):
+		if arr[cur_idx] < arr[cur_idx-1]: 
+			arr[cur_idx], arr[cur_idx-1] = arr[cur_idx-1], arr[cur_idx]
+
+print(f'sorted array {arr}')
